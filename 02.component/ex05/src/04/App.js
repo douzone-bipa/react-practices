d29f8081e0db00af1ce83ee1fec697f6c560685c
더존-부산정보산업진흥원 <90119630+douzone-bipa@users.noreply.github.com>
2021-12-05 12:07:00 +0900

    Update App.js

--- 02.component/ex05/src/04/App.js
+++ 02.component/ex05/src/04/App.js
@@ -3,13 +3,31 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    const getCurrentClockTime = () => {
+        const date = new Date();
+        const hours = date.getHours();
+        const minutes = ('0' + date.getMinutes()).slice(-2);
+        const seconds = ('0' + date.getSeconds()).slice(-2);
 
+        return {
+            hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+            minutes: minutes,
+            seconds: seconds,
+            session: hours >= 12 ? "pm" : "am"
+        };
+    }
 
-    return (
-            <Clock
+    const [state, setState] = useState(getCurrentClockTime());
+
+    useEffect(() => {
+        setInterval(function () {
+            setState(getCurrentClockTime());
+        }, 1000);
+    }, []);
+
+    return <Clock
                 message={'ex05: useEffect Hook example'}
                 hours={state.hours}
                 minutes={state.minutes}
-                seconds={state.seconds}/>
-    );
+                seconds={state.seconds} />;
 }
